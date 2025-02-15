812721d8a32314f5a49b094bed2ef21dabeb6953
BugHunter-X <askzenai@gmail.com>
2025-02-15 14:32:06 +0100

    Add firebaseBug.js file

---
+++ firebaseBug.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SEND_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+//Additional Firebase Services
+//const db = getFirestore(app); //If using Firestore
+//const storage = getStorage(app); //If using Storage
